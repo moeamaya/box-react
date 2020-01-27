9494d99574d7d91fbcd436c89fc371740a63a104
Moe Amaya <moeamaya@gmail.com>
2020-01-27 10:01:39 -0800

    Add dymanic inputs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [width, setWidth] = useState(400);
+  const [height, setHeight] = useState(600);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <label>Width</label>
+          <input
+            type="number"
+            value={width}
+            onChange={e => setWidth(e.target.value)}
+          ></input>
+          <label>Height</label>
+          <input
+            type="number"
+            value={height}
+            onChange={e => setHeight(e.target.value)}
+          ></input>
+        </div>
       </header>
+
+      <main className="App-main">
+        <div
+          className="box"
+          style={{ width: `${width}px`, height: `${height}px` }}
+        ></div>
+      </main>
     </div>
   );
 }
